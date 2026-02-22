b6c0c6a9d207f87f39a1214f5bde9956cbb53ab1
Slavka Makarov <86533092+Foxik-9322@users.noreply.github.com>
2026-02-22 11:02:29 +0300

    Update poems.js

--- poems.js
+++ poems.js
@@ -9,26 +9,68 @@ const bookData = {
 	},
 	"poems": [
 		{
-			"title": "A fox in a hat,<br>so dapper and keen,",
-			"content": "Walks through a joyful green.<br>With a skip and a hop,<br>tales to share,<br>He sang to one,<br>but to ears!",
+			"title": "First poem",
+			"content": "Strolling slowly through the show<br>I saw a frozen berry<br>I took it gently in my hands<br>And seized it warmly warmly<br>It melted soon right before my eyes.<br>Becoming soft and teasty<br>And I decided to eat it up<br>While mother spoke with a lady.",
 			"audio": true,
 			"styles": {}
 		},
 		{
 			"title": "Second poem",
-			"content": 'We invited Father Frost<br>To our Christmas Party<br>We put all snowballs in the box<br>And started sliding, sliding<br>"Hello, my children!" said Father Frost<br>"How are you? Hope fine and happy?"<br>We made a circle said "of course!"<br>And started our party!',
+			"content": "We invited Father Frost<br>To our Christmas Party<br>We put all snowballs in the box<br>And started sliding, sliding<br>\"Hello, my children!\" said Father Frost<br>\"How are you? Hope fine and happy?\"<br>We made a circle said \"of course!\"<br>And started our party!",
 			"audio": true,
 			"styles": {}
 		},
 		{
 			"title": "Third poem",
-			"content": "Продолжаем сказку,<br>Лисица идет дальше.",
+			"content": "I’m drawing a snowman,<br>Snowflakes and the sun<br>I want to touch them<br>And hug them like my mom.",
 			"audio": true,
 			"styles": {}
 		},
 		{
 			"title": "Fourth poem",
-			"content": "Конец истории,<br>Все по домам.",
+			"content": "Let’s wish up a marry Christmas!<br>Let’s wish up a marry Christmas!<br>Let’s wish up a marry Christmas!<br>And wonderful, bright days!",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Fifth poem",
+			"content": "Merry Christmas, merry Christmas!<br>Shout all children, teachers, pictures!<br>We are happy see each other<br>Smiling, brightly, hugging, tightly!",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Sixth poem",
+			"content": "",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Seventh poem",
+			"content": "Light up Christmas tree right now!<br>Holiday is coming!<br>Put all presents under tree<br>And turn on Carrols singing!<br>Bake a chocolate orange pie!<br>Put a candle on it!<br>Invite your parents and all friends<br>And start enjoying everything!",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Eighth poem",
+			"content": "Mummy, mummy<br>Yes, Ainur?<br>Buy me a sledge for the school<br>I will take my friends to school<br>And we’ll get good marks at school",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Ninth poem",
+			"content": "Look! The weather’s so beautiful this day!<br>The snow is everywhere!<br>The children’re playing in the yard<br>And making snowballs there!<br>Andrey made ten.<br>Ainur made twelve<br>??? made fifty there!<br>Now wonderful the winter day!<br>The snow falls everywhere!",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Tenth poem",
+			"content": "I put on my warm hat<br>I put on my boots<br>I put on my long scarf<br>And woollen puts on<br>\"Where’re you going to?\" cries out my beautiful mum<br>I’ll come back as soon as the sun goes down!",
+			"audio": true,
+			"styles": {}
+		},
+		{
+			"title": "Eleventh poem",
+			"content": "Sleighing, sleighing up and down<br>I saw some squirrels swishing<br>\"Come up!\" I said<br>\"And take the nuts that granny cracked for baby!\"<br>One, two, three, four, five, six, seven<br>Magic is everywhere!<br>Take your wand and wave it up<br>And you’ll get a gift for that!",
 			"audio": true,
 			"styles": {}
 		}
